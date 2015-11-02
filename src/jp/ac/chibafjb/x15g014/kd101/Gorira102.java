92de22c4205faefc17c72a1c4ac8adaf1035a4b9
unknown <x15g014@PC504N018.chiba-fjb.ac.jp>
2015-11-02 14:25:32 +0900

    Gorira誕生

---
+++ src/jp/ac/chibafjb/x15g014/kd101/Gorira102.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g014.kd101;
+
+public class Gorira102 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i <= 10;i++)
+			System.out.println(i + "船橋 太郎");
+
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g014/kd101/Gorira104.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g014.kd101;
+
+public class Gorira104 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
