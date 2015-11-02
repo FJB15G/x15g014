36f8f2fb55c78eb1336f550adcfe94b13e02ebd0
unknown <x15g014@PC504N018.chiba-fjb.ac.jp>
2015-11-02 14:55:08 +0900

    １０５

---
+++ src/jp/ac/chibafjb/x15g014/kd101/Gorira105.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g014.kd101;
+
+public class Gorira105 {
+
+	public static void main(String[] args) {
+		System.out.println("氏名：　石岡・G・ゴリラ（いしおか・ごり・ごりら）");
+		System.out.println("好物：　バナナ");
+		System.out.println("自己紹介：　ウフォッウフォフォッウフォフォッフォ");
+		
+
+	}
+
+}
