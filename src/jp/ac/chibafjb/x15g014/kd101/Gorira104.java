77e2de9108fb8bce701bd02d69ce28a56c392510
unknown <x15g014@PC504N018.chiba-fjb.ac.jp>
2015-11-02 14:33:06 +0900

    no message

--- src/jp/ac/chibafjb/x15g014/kd101/Gorira104.java
+++ src/jp/ac/chibafjb/x15g014/kd101/Gorira104.java
@@ -3,7 +3,9 @@ package jp.ac.chibafjb.x15g014.kd101;
 public class Gorira104 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		for(a=1;a<=3;a++)
+		System.out.println("あいうえお　あいうえお　あいうえお");
 
 	}
 
